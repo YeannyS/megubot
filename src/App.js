2614596368ce242f2897068114d728639ae87066
YeannyS <chaitawat2423@gmail.com>
2024-06-06 10:33:07 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,100 +1,76 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 
 const liff = window.liff;
 
-class App extends Component {
+const App = () => {
+  const [name, setName] = useState('');
+  const [userLineID, setUserLineID] = useState('');
+  const [pictureUrl, setPictureUrl] = useState('');
+  const [statusMessage, setStatusMessage] = useState('');
 
-  constructor(props) {
-    super(props);
+  const getProfile = async () => {
+    await liff.init();
+    let getProfile = await liff.getProfile();
+    setName(getProfile.displayName);
+    setUserLineID(getProfile.userId);
+    setPictureUrl(getProfile.pictureUrl);
+    setStatusMessage(getProfile.statusMessage);
+  };
 
-    this.state = {
-      name: '',
-      userLineID: '',
-      pictureUrl: '',
-      statusMessage: ''
-    };
-  }
-
-  getProfile() {
-    liff.init(async () => {
-      let getProfile = await liff.getProfile();
-      this.setState({
-        name: getProfile.displayName,
-        userLineID: getProfile.userId,
-        pictureUrl: getProfile.pictureUrl,
-        statusMessage: getProfile.statusMessage
-      });
-    });
-  }
-
-  sendMessage() {
+  const sendMessage = () => {
     liff.sendMessages([{
       type: 'text',
       text: "Hi LIFF"
     }]).then(() => {
       liff.closeWindow();
     });
-  }
+  };
 
-  closeLIFF() {
+  const closeLIFF = () => {
     liff.closeWindow();
-  }
+  };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div className="support">
-            <img width="25%" src="https://img.icons8.com/color/420/line-me.png" />
-            <img width="25%" src="https://lh3.googleusercontent.com/illfpW97yh9TtvtmtN-BiNcpomys5gzAj4nw8Je6Ydby814PRquAPcvsP2tAV43Iqe8logzjUnjp7tN5Dvk" />
-          </div>
-          <div className="support">
-            {
-              (this.state.pictureUrl && this.state.pictureUrl != '')
-                ?
-                <img width="25%" src={this.state.pictureUrl} />
-                :
-                null
-            }
-          </div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="support">
+          <img width="25%" src="https://img.icons8.com/color/420/line-me.png" />
+          <img width="25%" src="https://lh3.googleusercontent.com/illfpW97yh9TtvtmtN-BiNcpomys5gzAj4nw8Je6Ydby814PRquAPcvsP2tAV43Iqe8logzjUnjp7tN5Dvk" />
+        </div>
+        <div className="support">
           {
-            (this.state.name && this.state.name != '')
-              ?
-              <p>Name: {this.state.name}</p>
-              :
-              null
+            pictureUrl && pictureUrl !== '' &&
+            <img width="25%" src={pictureUrl} />
           }
-          {
-            (this.state.userLineID && this.state.userLineID != '')
-              ?
-              <p>LineID: {this.state.userLineID}</p>
-              :
-              null
-          }
-          {
-            (this.state.statusMessage && this.state.statusMessage != '')
-              ?
-              <p>statusMessage: {this.state.statusMessage}</p>
-              :
-              null
-          }
-          <div className="support">
-            <Button variant="contained" onClick={this.getProfile.bind(this)} style={{ marginRight: '20px' }} color="primary">
-              Getdata INFO
-            </Button>
-            <Button variant="contained" onClick={this.sendMessage.bind(this)} style={{ marginRight: '20px' }}>
-              Send Message
-            </Button>
-            <Button variant="contained" onClick={this.closeLIFF.bind(this)} color="secondary">
-              Close LIFF
-            </Button>
-          </div>
-        </header>
-      </div>
-    );
-  }
-}
+        </div>
+        {
+          name && name !== '' &&
+          <p>Name: {name}</p>
+        }
+        {
+          userLineID && userLineID !== '' &&
+          <p>LineID: {userLineID}</p>
+        }
+        {
+          statusMessage && statusMessage !== '' &&
+          <p>statusMessage: {statusMessage}</p>
+        }
+        <div className="support">
+          {/* <Button variant="contained"  style={{ marginRight: '20px' }} color="primary">
+            Getdata INFO
+          </Button>
+          <Button variant="contained"  style={{ marginRight: '20px' }}>
+            Send Message
+          </Button>
+          <Button variant="contained"  color="secondary">
+            Close LIFF
+          </Button> */}
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default App;
